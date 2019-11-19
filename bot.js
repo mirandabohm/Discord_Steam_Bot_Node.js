2345e425a9a65375c32543d509fe875f02b8166f
Upquark00 <miranda.bohm@yahoo.com>
2019-11-18 21:09:04 -0800

    Create basic bot skeleton. Update gitignore

--- bot.js
+++ bot.js
@@ -3,11 +3,20 @@ Intuitive bot that checks user status across multiple digital platforms.
     @author: Upquark00
 */
 
+var bot_token = require('./credentials')
+
 const Discord = require('discord.js');
-const client = new Discord.Client()
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Connected as ' + client.user.tag)
-    }
-    );
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login('token');
+console.log(bot_token)
