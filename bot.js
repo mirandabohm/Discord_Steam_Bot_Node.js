c756ff35a06006f4a14c3cb090b5d37d4067642a
Upquark00 <miranda.bohm@yahoo.com>
2019-11-20 23:06:28 -0800

    Update client.on.

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
-/* First draft authored on Tues Nov 16 19:06:00 2019
+/** First draft authored on Tues Nov 16 19:06:00 2019
 Intuitive bot that checks user status across multiple digital platforms.
     @author: Upquark00
 */
 
 var bot_token = require('./credentials')
 
+// Open a Discord client session
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -13,10 +14,10 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.content.startsWith('!hello')) {
+        msg.channel.send(`Hello ${msg.author}`);
     }
 });
 
-client.login('token');
+client.login(bot_token);
 console.log(bot_token)
